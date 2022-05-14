a1d6b635729c8df43b46481b420b123a7f1b0e9e
illusion173 <illusion173@hotmail.com>
2022-05-14 15:28:47 -0400

    starting some new stuff

---
+++ shorelark/libs/neural-network/src/lib.rs
@@ -0,0 +1,30 @@
+#[cfg(test)]
+
+pub struct Network{
+    layers: Vec<Layer>;
+}
+
+
+struct Layer;
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
+
+impl Network{
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32>{
+        todo!()
+    }
+}
+
+
+int get_berry_number(){
+
+}
+
+fn berry_number() -> usize {
+    return ();
+}
